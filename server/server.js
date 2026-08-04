1528907f5d56052356db6d0fe27b4d1b9a748198
Subhashini <subhashini1089@gmail.com>
2026-08-04 23:04:52 +0530

    checkk

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // ===============================
-// Database Connection Check
+// Database Connection Checkkkk
 // ===============================
 
 pool
